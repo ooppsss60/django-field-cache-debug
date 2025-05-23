0122289141db3948972ed98860e044e7b9a6744a
Дмитрий Головнёв <dmitry.golovnev@initflow.com>
2025-05-23 14:58:57 +0300

    Fix

--- field_cache_debug/apps.py
+++ field_cache_debug/apps.py
@@ -1,9 +1,10 @@
-from doctest import debug
+from django.apps import AppConfig
 
 
 
 class FieldCacheDebugConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
+    name = "field_cache_debug"
 
     def ready(self):
         from field_cache_debug import debug # NOQA
